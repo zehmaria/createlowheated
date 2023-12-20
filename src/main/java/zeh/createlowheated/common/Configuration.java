3bb532fafb122f2e2885425d213fb03e25d83386
Nereithr <72263884+Nereithr@users.noreply.github.com>
2023-12-20 19:06:29 +0300

    Add support for Hot Burners, sensibly

--- src/main/java/zeh/createlowheated/common/Configuration.java
+++ src/main/java/zeh/createlowheated/common/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
 
 	public static ForgeConfigSpec.IntValue FAN_MULTIPLIER;
 	
-	public static ForgeConfigSpec.IntValue HOT_BURNERS_MULTIPLIER;
+	public static ForgeConfigSpec.IntValue BASE_MULTIPLIER;
 	
 	public static ForgeConfigSpec.BooleanValue HOT_BURNERS;
 
@@ -20,13 +20,13 @@ public class Configuration {
 
 		COMMON_BUILDER.comment("#Charcoal Burner Requirements").push("charcoal_burner");
 		
-		HOT_BURNERS_MULTIPLIER = COMMON_BUILDER.comment("How much more a burner consumes when the hotBurners option is active. Does not affect burners empowered by fans, use fanMultiplier instead.")
-				.defineInRange("hotBurnersMultiplier", 2, 1, Integer.MAX_VALUE);
-		
 		HOT_BURNERS = COMMON_BUILDER.comment("When set to True, an active Charcoal Burner produces the same heat as a Kindled Blaze Burner and an empowered Charcoal Burner produces the same heat as a Seething Blaze Burner. Inactive state is unaffected.")
 				.define("hotBurners", false);
 		
-		FAN_MULTIPLIER = COMMON_BUILDER.comment("How much more a burner consumes when empowered by a maxed encased fan.")
+		BASE_MULTIPLIER = COMMON_BUILDER.comment("How much more fuel a non-empowered Charcoal Burner consumes. Use fanMultiplier for fan-empowered burners. Intended for use with the hotBurners option, the default value of 1 is recommended otherwise.")
+				.defineInRange("baseMultiplier", 1, 1, Integer.MAX_VALUE);
+		
+		FAN_MULTIPLIER = COMMON_BUILDER.comment("How much more fuel a Charcoal Burner consumes when empowered by a maxed encased fan.")
 				.defineInRange("fanMultiplier", 100, 1, Integer.MAX_VALUE);
 
 		COMMON_BUILDER.pop();
