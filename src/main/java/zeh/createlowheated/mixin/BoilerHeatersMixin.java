d044e3d40432fba091c440649bbf49388bd011c5
zehmaria <10835511+zehmaria@users.noreply.github.com>
2024-05-03 14:25:59 -0300

    adds an option to disable the heating of boilers

--- src/main/java/zeh/createlowheated/mixin/BoilerHeatersMixin.java
+++ src/main/java/zeh/createlowheated/mixin/BoilerHeatersMixin.java
@@ -5,19 +5,20 @@ import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
 import com.simibubi.create.content.processing.burner.BlazeBurnerBlock.HeatLevel;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import zeh.createlowheated.AllBlocks;
+import zeh.createlowheated.common.Configuration;
 import zeh.createlowheated.content.processing.basicburner.BasicBurnerBlock;
 
 @Mixin(value = BoilerHeaters.class, remap = false)
 public class BoilerHeatersMixin {
-    /**
-     * @author ZehMaria
-     * @reason Adds Basic Burner, removes all passive heaters.
-     */
-    @Overwrite
-    public static void registerDefaults() {
+    @Inject(method = "registerDefaults", at = @At("HEAD"), cancellable = true)
+    private static void registerDefaultsMixin(CallbackInfo ci) {
+        if (!Configuration.BASIC_BURNER_BOILER.get()) return;
+
         registerHeater(com.simibubi.create.AllBlocks.BLAZE_BURNER.get(), (level, pos, state) -> {
             HeatLevel value = state.getValue(BlazeBurnerBlock.HEAT_LEVEL);
             if (value == HeatLevel.NONE) return -1;
@@ -35,23 +36,9 @@ public class BoilerHeatersMixin {
             return -1;
         });
 
-        registerHeaterProvider((level, pos, state) -> {
-            //if (AllTags.AllBlockTags.PASSIVE_BOILER_HEATERS.matches(state)) return (level1, pos1, state1) -> 0;
-            return null;
-        });
+        ci.cancel();
     }
-
-    @Shadow public static void registerHeaterProvider(BoilerHeaters.HeaterProvider provider) {}
 
     @Shadow public static void registerHeater(Block block, BoilerHeaters.Heater heater) {}
-
-    /* For future reference
-    @Inject(method = "apply(Lcom/simibubi/create/content/processing/basin/BasinBlockEntity;Lnet/minecraft/world/item/crafting/Recipe;Z)Z",
-            at = @At(value = "NEW", target = "()Ljava/util/ArrayList;", ordinal = 0), cancellable = true)
-    private static void onApplyNew(BasinBlockEntity basin, Recipe<?> recipe, boolean test, CallbackInfoReturnable<Boolean> info) {
-        //HeatLevel heat = BasinBlockEntity.getHeatLevelOf(basin.getLevel().getBlockState(basin.getBlockPos().below(1)));
-        //if (recipe instanceof BasinRecipe && !((BasinRecipe) recipe).getRequiredHeat().testBlazeBurner(heat)) info.setReturnValue(false);
-    }
-     */
 
 }
