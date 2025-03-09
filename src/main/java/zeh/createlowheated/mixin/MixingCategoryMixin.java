64668a39649f93db7f577c4efaabf14b27b724c2
zehmaria <10835511+zehmaria@users.noreply.github.com>
2025-03-09 04:06:25 -0300

    es

--- src/main/java/zeh/createlowheated/mixin/MixingCategoryMixin.java
+++ src/main/java/zeh/createlowheated/mixin/MixingCategoryMixin.java
@@ -20,7 +20,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;    
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
